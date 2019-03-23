9b33b80b99bd9d710e46f48a480afadd05e7ddcb
Liang <392425595@qq.com>
2019-03-23 21:47:31 +0800

    [LLR][1.Drama-group-billing][查询取代临时变量 playFor(aPerformance)--->play参数]

--- Drama-group-billing/Refactoring/printDramaBillingDetails.js
+++ Drama-group-billing/Refactoring/printDramaBillingDetails.js
@@ -1,4 +1,30 @@
 function statement (invoice, plays) {
+  function amountFor (aPerformance) {
+    const play = playFor(perf);
+    let result = 0;
+    switch (play.type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`unknown type: ${play.type}`)
+    }
+    return result;
+  }
+  function playFor (aPerformance) {
+    return plays[aPerformance.playId];
+  }
+
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`
@@ -9,8 +35,8 @@ function statement (invoice, plays) {
     }).format
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID]
-    let thisAmount = amountFor(perf, play);
+    const play = playFor(perf);
+    let thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0)
@@ -25,26 +51,4 @@ function statement (invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
   return result
-}
-
-function amountFor (aPerformance, play) {
-  let result = 0;
-  switch (play.type) {
-    case 'tragedy':
-      result = 40000
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30)
-      }
-      break
-    case 'comedy':
-      result = 30000
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20)
-      }
-      result += 300 * aPerformance.audience
-      break
-    default:
-      throw new Error(`unknown type: ${play.type}`)
-  }
-  return result;
 }
