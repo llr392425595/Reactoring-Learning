8fbd9c2c25090e70ec5cbf5a20340d568df1c5cf
Liang <392425595@qq.com>
2019-05-09 04:07:04 +0800

    [LLR][1.Drama-group-billing][分隔文件，将不同的计算逻辑完全分离]

---
+++ Drama-group-billing/Refactoring/conmmon.js
@@ -0,0 +1,8 @@
+export default function usd (aNumber) {
+  return new Intl.NumberFormat('en-US',
+    {
+      style: 'currency', currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100)
+}
+
--- Drama-group-billing/Refactoring/printDramaBillingDetails.js
+++ Drama-group-billing/Refactoring/printDramaBillingDetails.js
@@ -1,79 +1,35 @@
+import createStatementData from './createStatementData'
+import usd from './conmmon'
+
 function statement (invoice, plays) {
-  const statementData = {}
-  statementData.customer = invoice.customer
-  statementData.performances = invoice.performances.map(enrichPerformance)
-  statementData.totalAmount = totalAmount(statementData)
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
-  return renderPlainText(statementData, invoice, plays)
-
-  function enrichPerformance (aPerformance) {
-    const result = Object.assign({}, aPerformance)
-    result.play = playFor(aPerformance)
-    result.amount = amountFor(aPerformance)
-    result.volumeCredits = volumeCreditsFor(aPerformance)
-    return result
-  }
-
-  function playFor (aPerformance) {
-    return plays[aPerformance.playId]
-  }
-
-  function amountFor (aPerformance) {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`)
-    }
-    return result
-  }
-
-  function volumeCreditsFor (aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ('comedy' === aPerformance.play.type) result += Math.floor(
-      aPerformance.audience / 5)
-    return result
-  }
-
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0)
-  }
-
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
-  }
+  return renderPlainText(createStatementData(invoice, plays))
 }
 
 function renderPlainText (data) {
-
-  function usd (aNumber) {
-    return new Intl.NumberFormat('en-US',
-      {
-        style: 'currency', currency: 'USD',
-        minimumFractionDigits: 2,
-      }).format(aNumber / 100)
-  }
-
   let result = `Statement for ${data.customer}\n`
-
   for (let perf of data.performances) {
     result += `  ${pref.play.name}: ${usd(pref.amount)} (${perf.audience} seats)\n`
   }
-
   result += `Amount owed is ${usd(data.totalAmount)}\n`
   result += `You earned ${data.totalVolumeCredits} credits\n`
   return result
+}
+
+
+function htmlStatement (invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays))
+}
+
+function renderHtml (data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
 }
